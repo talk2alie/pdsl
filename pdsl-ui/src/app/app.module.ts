b5c9a31809b4b895b8758ad503a44fcdb97ec397
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2021-10-21 19:24:29 -0400

    Add home component

--- pdsl-ui/src/app/app.module.ts
+++ pdsl-ui/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
