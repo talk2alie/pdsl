83b13b9de566a64e661b59da92b07773242a6bf1
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2022-04-07 19:35:28 -0400

    Add new press release

--- pdsl-ui/src/app/services/press-release.data.ts
+++ pdsl-ui/src/app/services/press-release.data.ts
@@ -2,48 +2,12 @@ import { PressRelease } from './press-release.model';
 
 export const pressReleases: PressRelease[] = [
     {
-        title: 'Welcome to the New Age',
+        title: 'Conclusion of Direct Negotiation Licensing Modality',
         heroImage: '/assets/press-releases/hero.jpg',
         description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'The Petroleum Directorate of Sierra Leone (hereinafter referred to as PDSL) wishes to inform the stake-holding community that it is on the verge of concluding its Exploration and Production (E&P) license award via Direct Negotiation. PDSL is determining the efficacy of its options for E&P license award under prevailing circumstances.',
         documentLink:
-            '/assets/applications/1 Pre-Qualification Guidelines For Investors.pdf',
-        releaseDate: new Date('2021-12-12'),
-    },
-    {
-        title: 'International Deligate Petroleum Directorate Director',
-        heroImage: '/assets/press-releases/hero.jpg',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        documentLink:
-            '/assets/applications/1 Pre-Qualification Guidelines For Investors.pdf',
-        releaseDate: new Date('2022-01-15'),
-    },
-    {
-        title: 'Oil Discovered in Makeni',
-        heroImage: '/assets/press-releases/hero.jpg',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        documentLink:
-            '/assets/applications/1 Pre-Qualification Guidelines For Investors.pdf',
-        releaseDate: new Date('2021-12-15'),
-    },
-    {
-        title: "President Bio Recognizes Petroleum Directrate's International Work",
-        heroImage: '/assets/press-releases/hero.jpg',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        documentLink:
-            '/assets/applications/1 Pre-Qualification Guidelines For Investors.pdf',
-        releaseDate: new Date('2021-12-22'),
-    },
-    {
-        title: 'Sierra Leone Signs Oil Partnership with Liberia',
-        heroImage: '/assets/press-releases/hero.jpg',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        documentLink:
-            '/assets/applications/1 Pre-Qualification Guidelines For Investors.pdf',
-        releaseDate: new Date('2022-02-12'),
+            '/assets/press-releases/Conclusion of Direct Negotiations.pdf',
+        releaseDate: new Date('April 7 2022'),
     },
 ];
