c3ba17400b36776ffc09baaf521ae26017bb9346
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2022-06-12 07:53:17 +0000

    Complete first pass of pdsl api service

--- pdsl-ui/src/app/services/pdsl.api.service.ts
+++ pdsl-ui/src/app/services/pdsl.api.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RegisterVisitorViewModel } from './register-visitor.model';
+import { Observable } from 'rxjs';
+import { VisitorOutputViewModel } from './visitor-output.model';
+import { VerifyCodeViewModel } from './verify-code.model';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class Pdsl.ApiService {
+@Injectable({ providedIn: 'root' })
+export class PdslApiService {
+    private baseUrl = 'https://localhost:7006';
 
-  constructor() { }
+    constructor(private httpClient: HttpClient) {}
+
+    registerVisitor(
+        visitor: RegisterVisitorViewModel
+    ): Observable<VisitorOutputViewModel> {
+        let url = `${this.baseUrl}/send`;
+        return this.httpClient.post<VisitorOutputViewModel>(url, visitor);
+    }
+
+    verifyCode(visitorToVerify: VerifyCodeViewModel): Observable<VisitorOutputViewModel> {
+        let url = `${this.baseUrl}/verify`;
+        return this.httpClient.post<VisitorOutputViewModel>(url, visitorToVerify);
+    }
 }
