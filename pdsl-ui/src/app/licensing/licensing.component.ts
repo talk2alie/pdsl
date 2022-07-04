10cd3a55628f42bf8da33fedf53845aca3b2f19e
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2022-07-04 19:41:03 -0400

    Add code from previous branch to licensing branch

---
+++ pdsl-ui/src/app/licensing/licensing.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RegisterVisitorOutputViewModel } from '../shared/register-visitor.model';
+import { VerifyCodeVisitorOutputViewModel } from '../shared/verify-code.model';
+
+@Component({
+    selector: 'pdsl-licensing',
+    templateUrl: './licensing.component.html',
+    styleUrls: ['./licensing.component.css'],
+})
+export class LicensingComponent implements OnInit {
+    identityFormSubmitted = false;
+    identityVerified = false;
+
+    submittedVisitor?: RegisterVisitorOutputViewModel = undefined;
+
+    errorMessage?: string = undefined;
+
+    constructor(private titleService: Title) {
+        this.titleService.setTitle('PDSL | Licensing');
+    }
+
+    ngOnInit(): void {
+        this.errorMessage = undefined;
+    }
+
+    onIdentitySubmitted(visitorSubmittedModel: RegisterVisitorOutputViewModel): void {
+        if(!visitorSubmittedModel.isCodeVerified) {
+            this.submittedVisitor = visitorSubmittedModel;
+        }
+
+        this.identityFormSubmitted = visitorSubmittedModel.isCodeSent;
+        this.identityVerified = visitorSubmittedModel.isCodeVerified;
+    }
+
+    onIdentityVerified(vierifiedVisitor: VerifyCodeVisitorOutputViewModel): void {
+        this.identityVerified = vierifiedVisitor.isCodeVerified;
+        if(!this.identityVerified) {
+            this.submittedVisitor = undefined;
+            this.identityFormSubmitted = false;
+            this.errorMessage = 'The code you provided is either incorrect or is expired. Please retry submitting your information.';
+            return;
+        }
+
+        this.identityFormSubmitted = true;
+        this.errorMessage = undefined;
+    }
+}
